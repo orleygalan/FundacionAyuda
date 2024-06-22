fc21fd50392e0a230952e88ffa7f1d04f99c39a7
clon-Zillow <orleigalan@gmail.com>
2024-06-22 11:11:51 -0500

    -proceso completado.

--- src/AnalyticsTracker.jsx
+++ src/AnalyticsTracker.jsx
@@ -1,12 +1,13 @@
-import { getAnalytics, logEvent } from 'firebase/analytics';
+import { logEvent } from 'firebase/analytics';
 import { useEffect } from 'react';
 import { useLocation } from 'react-router';
+import { analytics } from './Component/confgSDK/SDK';
 
 const AnalyticsTracker = () => {
   const location = useLocation();
 
   useEffect(() => {
-    logEvent(getAnalytics, 'page_view', {
+    logEvent(analytics, 'page_view', {
       page_path: location.pathname,
       page_location: window.location.href,
       page_title: document.title,
